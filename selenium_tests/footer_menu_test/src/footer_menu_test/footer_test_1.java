e611e8b2a70d0ea1331a88c4c6b6e6cd23e43ec3
Jane Geryak <janegeryak@gmail.com>
2017-01-08 11:50:01 +0200

    footer_test_1

---
+++ selenium_tests/footer_menu_test/src/footer_menu_test/footer_test_1.java
@@ -0,0 +1,77 @@
+package footer_menu_test;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class footer_test_1 {
+	  private WebDriver driver;
+	  private String baseUrl;
+	  private boolean acceptNextAlert = true;
+	  private StringBuffer verificationErrors = new StringBuffer();
+
+	  @Before
+	  public void setUp() throws Exception {
+		System.setProperty("webdriver.gecko.driver", "D:\\test_enviroment\\geckodriver.exe");
+	    driver = new FirefoxDriver();
+	    baseUrl = "https://jennygeryak.github.io/";
+	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	  }
+
+	  @Test
+	  public void testUntitled() throws Exception {
+	    driver.get(baseUrl + "/index.html");
+	    driver.findElement(By.cssSelector("i.fa.fa-facebook-square")).click();
+	    driver.findElement(By.cssSelector("i.fa.fa-linkedin-square")).click();
+	    driver.findElement(By.cssSelector("i.fa.fa-instagram")).click();
+	    driver.findElement(By.cssSelector("i.fa.fa-github-square")).click();
+	  }
+
+	  @After
+	  public void tearDown() throws Exception {
+	    driver.quit();
+	    String verificationErrorString = verificationErrors.toString();
+	    if (!"".equals(verificationErrorString)) {
+	      fail(verificationErrorString);
+	    }
+	  }
+
+	  private boolean isElementPresent(By by) {
+	    try {
+	      driver.findElement(by);
+	      return true;
+	    } catch (NoSuchElementException e) {
+	      return false;
+	    }
+	  }
+
+	  private boolean isAlertPresent() {
+	    try {
+	      driver.switchTo().alert();
+	      return true;
+	    } catch (NoAlertPresentException e) {
+	      return false;
+	    }
+	  }
+
+	  private String closeAlertAndGetItsText() {
+	    try {
+	      Alert alert = driver.switchTo().alert();
+	      String alertText = alert.getText();
+	      if (acceptNextAlert) {
+	        alert.accept();
+	      } else {
+	        alert.dismiss();
+	      }
+	      return alertText;
+	    } finally {
+	      acceptNextAlert = true;
+	    }
+	  }
+	}
+
